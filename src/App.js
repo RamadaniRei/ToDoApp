8e1af77fb259e7dd67a2c5ceeb35fa324186bc23
Rei Ramadani <ramadanirei@gmail.com>
2024-08-20 12:16:27 +0200

    Adding New Note

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./components/Header";
 import CreateArea from "./components/CreateArea";
+import Note from "./components/Note";
 
 const App = () => {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes((prevNotes) => {
+      return [...prevNotes, newNote];
+    });
+    console.log(notes);
+  }
   return (
     <div>
       <Header />
-      <CreateArea />
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+          />
+        );
+      })}
     </div>
   );
 };
